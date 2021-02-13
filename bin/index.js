6fd9fae2479963e2c9adf6695a4c3608bced87de
David Heidelberger <david.heidelberger@gmail.com>
2021-02-13 02:02:47 -0500

    Added support for a custom distribution channel argument to the sign command

--- bin/index.js
+++ bin/index.js
@@ -5,19 +5,18 @@ const cmds = {
   switch: require("../lib/switch"),
   update: require("../lib/update"),
   register: require("../lib/register"),
-  sign: require("../lib/sign")
+  sign: require("../lib/sign"),
 };
 
 async function init() {
-
   // Object.keys(cmds).forEach(action => {
   //   if (new RegExp(`${action}`).test(process.argv)) cmds[action]();
   // });
-  if (/help/.test(process.argv)) cmds.help()
-  else if (/switch/.test(process.argv)) cmds.switch()
-  else if (/update/.test(process.argv)) cmds.update()
-  else if (/register/.test(process.argv)) cmds.register()
-  else if (/sign/.test(process.argv)) cmds.sign();
+  if (/help/.test(process.argv)) cmds.help();
+  else if (/switch/.test(process.argv)) cmds.switch();
+  else if (/update/.test(process.argv)) cmds.update();
+  else if (/register/.test(process.argv)) cmds.register();
+  else if (/sign/.test(process.argv)) cmds.sign(process.argv);
 }
 
 init();
